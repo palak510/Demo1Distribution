f1a0d1b66aa645248826f13d1b55fe9f88614afc
palak510 <palak.garg@thoughtworks.com>
2023-09-05 12:29:56 +0530

    Create spm for demo1 framework

--- Package.swift
+++ Package.swift
@@ -3,8 +3,15 @@
 
 import PackageDescription
 
+let version = "1.0.0"
+let moduleName = "Demo1Module"
+let checksum = "8f42f67a0665798e1c4e149c73a36fb9dddc062bc90e3b4e077ea571613ddda1"
+
 let package = Package(
     name: "Demo1Distribution",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -18,11 +25,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Demo1Distribution",
-            dependencies: []),
-        .testTarget(
-            name: "Demo1DistributionTests",
-            dependencies: ["Demo1Distribution"]),
+        .binaryTarget(
+            name: moduleName,
+            url: "https://github.com/litoarias/NetworkModuleDistribution/releases/download/\(version)/\(moduleName).xcframework.zip",
+            checksum: checksum
+        )
     ]
 )
